b57155306f05178dc400e3cbe570dbcb0bcd09f5
anameilani <meilani.ana@gmail.com>
2019-08-23 10:19:29 +0700

    crud planning-outcome

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router= require('express').Router()
 const planRoute= require('./planRoute')
+const outcomeRoute= require('./outcomeRoute')
 
 router.use('/plan', planRoute)
+router.use('/outcome', outcomeRoute)
 
 
 module.exports= router
---
+++ routes/outcomeRoute.js
@@ -0,0 +1,9 @@
+const router= require('express').Router()
+const outcomeController= require('../controllers/outcomeController')
+
+router.get('/:id', outcomeController.findOne)
+router.post('/:id', outcomeController.create)
+router.patch('/:id', outcomeController.findOne)
+router.delete('/:id', outcomeController.delete)
+
+module.exports= router
