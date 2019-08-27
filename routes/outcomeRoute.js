3b3a7ccc9588aefa67f5b7e69e3ef60a0d9f8b5f
viutytiadita <48216032+viutytiadita@users.noreply.github.com>
2019-08-27 07:00:48 +0700

    Revert "Usertesting"

--- routes/outcomeRoute.js
+++
@@ -1,9 +0,0 @@
-const router= require('express').Router()
-const outcomeController= require('../controllers/outcomeController')
-
-router.get('/:id', outcomeController.findOne)
-router.post('/', outcomeController.create)
-router.patch('/:id', outcomeController.update)
-router.delete('/:id', outcomeController.remove)
-
-module.exports= router
